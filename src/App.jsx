feae26ae2d45e75ca85df85170b99492dbca0f2e
riyaz shaikh <riyaz.s@osplabs.com>
2023-09-28 12:50:00 +0530

    added some new styles, completed delete part of todo

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import TodosList from './components/todos/TodosList';
 const App = () => {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
 
   return (
     <div className='container'>
       <div className="app-wrapper">
         <Header />
-        <Input input={input} setInput={setInput} todos={todos} setTodos={setTodos} />
-        <TodosList todos={todos} setTodos={setTodos} />
+        <Input input={input} setInput={setInput} todos={todos} setTodos={setTodos} editTodo={editTodo} setEditTodo={setEditTodo} />
+        <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
       </div>
     </div>
   )
--- src/components/todos/TodosList.jsx
+++ src/components/todos/TodosList.jsx
@@ -1,19 +1,28 @@
 const TodosList = ({ todos, setTodos }) => {
+    const onCompleteHandler = ({ id, completed }) => {
+        setTodos(prevTodos => prevTodos.map((item) => item.id === id ? { ...item, completed: !completed } : item))
+    }
+    const onEditHandler = () => { }
+
+    const onDeleteHandler = ({ id }) => {
+        setTodos(prevTodos => prevTodos.filter((item) => item.id !== id));
+    }
     return (
         <div>
             {todos.map((todo) => (
                 <li className="list-item" key={todo.id}>
                     <input type="text" name="todoslist" id="todoslist" value={todo.title} className="list" onChange={(e) => e.preventDefault()} />
                     <div className="icons">
-                        <button className="button-complete task-button">
+                        <button className="button-complete task-button" onClick={() => onCompleteHandler(todo)}>
                             <i className="fa fa-check-circle"></i>
                         </button>
-                        <button className="button-edit task-button">
+                        <button className="button-edit task-button" onClick={() => onEditHandler()}>
                             <i className="fa fa-edit"></i>
                         </button>
-                        <button className="button-delete task-button">
+                        <button className="button-delete task-button" onClick={() => onDeleteHandler(todo)}>
                             <i className="fa fa-trash"></i>
                         </button>
+                        <p>{console.log('todo', todo.id)}</p>
                     </div>
                 </li>
             ))
